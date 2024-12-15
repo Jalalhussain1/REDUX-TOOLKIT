eb91d62d76fd7f4eb2ccad2fa1c2b78930459dac
JALAL HUSSAIN <125751908+Jalalhussain1@users.noreply.github.com>
2024-12-15 14:37:54 +0500

    complete the cards section

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CartsData from './CartData';
 
 const Home = () => {
+   const [CartData,setCartDat] = useState(CartsData)
   return (
-    <div>Home</div>
+    <>
+    <section className='items_section mt-4 container'>
+     <h2 className='px-4' style={{fontWeight:400}}>Restaurants in peshawar Open Now</h2>
+     <div className='row mt-2 d-flex justify-content-around align-items-center'>
+    
+      {
+        CartData.map((element,index)=> {
+          return(
+            <>
+              <Card style={{width:'22rem',border:'none'}} className='hove mb-4'>
+             <Card.Img variant='top' className='cd' src={element.imgdata} />
+             <div className='card_body'>
+              <div className='upper_data d-flex justify-content-between align-items-center'>
+                <h4 className='mt-2'>{element.dish }</h4>
+                <span> {element.rating}</span>
+              </div>
+              <div className='lower_data d-flex justify-content-between '>
+                <h5>{element.address}</h5>
+                <span>{element.price}</span>
+              </div>
+              <div className='extra'></div>
+              <div className='last_data d-flex justify-content-between align-items-center'>
+                  <img src={element.arrimg} className='limg '/>
+                  <Button style={{width:'150px',background:'#ff3054db',border:'none'}} variant='outline-light'
+                  className='mt-2 mb-2'
+                  >Add to Cart</Button>
+                  <img src={element.delimg} className='laimg '/>
+              </div>
+             </div>
+      </Card>
+            </>
+          )
+        })
+      }
+
+      
+     </div>
+    </section>
+    </>
   )
 }
 
